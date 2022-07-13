a1d328e2ee828bd951b28bad1860754483cc86dc
@Juliaoha <juliaoha@hotmail.com>
2022-07-13 10:53:55 +1000

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,10 @@ Rails.application.routes.draw do
   root "socks#index"
   resources :socks
   get 'about', to: 'pages#about'
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
+  resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
