2cf6b3f3b22cc8f7db02c3515a20d8c38626eef4
@Juliaoha <juliaoha@hotmail.com>
2022-07-15 16:44:34 +1000

    change min characters

--- app/models/sock.rb
+++ app/models/sock.rb
@@ -1,7 +1,7 @@
 class Sock < ApplicationRecord
     belongs_to :user
     has_one_attached :image
-    validates :title, presence: true, length: {minimum: 10, maximum: 100}
+    validates :title, presence: true, length: {minimum: 5, maximum: 100}
     validates :description, presence: true, length: {minimum: 10, maximum: 300}
     validates :size, presence: true
     validates :brand, presence: true
